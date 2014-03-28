d56a7e9ad141c40b3fb5a5fdb6743b75b6da4f02
azarateo <azarate@cool4code.com>
2014-03-28 15:39:07 -0500

    error de compilation

--- LectorXML/LectorXMLViewController.m
+++ LectorXML/LectorXMLViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LectorXMLViewController.h"
+//http://ipad.about.com/od/iPad-App-Dev/a/How-To-Parse-Xml-Files-In-Xcode-Objective-C.htm
 
 @interface LectorXMLViewController ()
 
@@ -18,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self analizaArchivoXMLEnURL:@"poi.colombiajoven.gov.co/api/oferta"];
 }
 
 -(void)analizadorInicioDocumento:(NSXMLParser *)analizador
@@ -57,12 +59,65 @@
 
 }
 
--(void)analizador:(NSXMLParser *)elanalizador ocurrioErrordeAnalisis:(NSError *)elerror{
+-(void) parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError{
 
-    NSString *enunciadoDeError = [NSString stringWithFormat:@"Número de error %i", [elerror code]];
+    NSString *enunciadoDeError = [NSString stringWithFormat:@"Número de error %i", [parseError code]];
     NSLog(@"Error ejecutando el análisis del XML %@", enunciadoDeError);
     errorDeAnalisis = YES;
 
+}
+
+
+-(void)     parser:(NSXMLParser *)parser
+   didStartElement:(NSString *)elementName
+      namespaceURI:(NSString *)namespaceURI
+     qualifiedName:(NSString *)qName
+        attributes:(NSDictionary *)attributeDict{
+    
+    elementoActual = [elementName copy];
+    valorDelElemento = [[NSMutableString alloc] init];
+    if([elementName isEqualToString:@"Oportunidad"]){
+        item = [[NSMutableDictionary alloc] init];
+    }
+
+}
+
+-(void)     parser:(NSXMLParser *)parser
+   foundCharacters:(NSString *)string{
+
+    [valorDelElemento appendString:string];
+    
+}
+
+
+-(void)     parser:(NSXMLParser *)parser
+     didEndElement:(NSString *)elementName
+      namespaceURI:(NSString *)namespaceURI
+     qualifiedName:(NSString *)qName{
+    
+    
+    NSLog(@"Nombre del elemento %@",elementName);
+     NSLog(@"Nombre del elemento %@",namespaceURI);
+     NSLog(@"Nombre del elemento %@",qName);
+    
+
+    if([elementName isEqualToString:@"Oportunidad"]){
+        [articulos addObject:[item copy]];
+    }else {
+        [item setObject:valorDelElemento forKey:elementName];
+    }
+
+}
+
+- (void)parserDidEndDocument:(NSXMLParser *)parser {
+    
+    if (errorDeAnalisis == NO)
+    {
+        NSLog(@"Procesamiento de XML terminado");
+    } else {
+        NSLog(@"Comenzó error durante el análisis");
+    }
+    
 }
 
 
